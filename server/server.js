a5c716ba3dfd7c0135c0067ca33b0075266b1d55
Ian Grzembski <iangrz2003@gmail.com>
2024-06-20 16:47:04 -0400

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,21 @@
-console.log("Hello, World!");
-console.log("Developing in the dev branch.");
-console.log('This change is in the main branch.');
-console.log('Another change in the dev branch.');
+import express from 'express';
+import { promises as fs } from 'fs';
+
+const app = express();
+const PORT = 3000;
+
+// Endpoint to read and send JSON file content
+app.get('/socks', async (_req, res) => {
+    try {
+        const data = await fs.readFile('../data/socks.json', 'utf8');
+        const jsonObj = JSON.parse(data);
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you!");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
