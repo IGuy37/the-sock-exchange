b5fba85ebc28ab120bc30c2631c8942db31d5335
Ian Grzembski <iangrz2003@gmail.com>
2024-06-28 15:50:08 -0400

    Moved API over to local MongoDB/Express server

--- server/server.js
+++ server/server.js
@@ -1,50 +1,63 @@
 import express from 'express';
 import { promises as fs } from 'fs';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import pg from 'pg';
+
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
 
 const app = express();
 const PORT = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cors());
 
 app.post('/socks', async (req, res) => {
     try {
-        // Obligatory reference to POST Malone
-        console.log("If POST Malone were a sock, he'd be the one with the most colorful pattern.");
-        // Simulate creating a user
-        const { username, email } = req.body;
-        if (!username || !email) {
-            // Bad request if username or email is missing
-            return res.status(400).send({ error: 'Username and email are required.' });
-        }
-
-        // Respond with the created user information and a 201 Created status
-        res.status(201).send({
-            status: 'success',
-            location: 'http://localhost:3000/users/1234', // This URL should point to the newly created user
-            message: 'User created successfully.'
-        });
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const result = await collection.insertOne(req.body);
+        console.log(`New document created with _id: ${result.insertedId}`);
+        client.close();
+        res.json(result);
     } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
     }
 });
 
+app.post('/socks/search', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        console.log(req.body);
+        const documents = await collection.find({"sockDetails.color": req.body.searchTerm}).toArray();
+        console.log(documents);
+        client.close();
+        res.json(documents);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error searching for socks');
+    }
+});
+
+
 // Endpoint to read and send JSON file content
 app.get('/socks', async (req, res) => {
     try {
-        // Console log the entire request object
-        console.log(req);
-
-        // Console log specific parts of the request
-        console.log("Headers:", req.headers);
-        console.log("URL:", req.url);
-        console.log("Method:", req.method);
-        console.log("Query parameters:", req.query);
-
-        const data = await fs.readFile('../data/socks.json', 'utf8');
-        const jsonObj = JSON.parse(data);
-        res.json(jsonObj);
+        console.log(url)
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const socks = await collection.find({}).toArray();
+        res.json(socks);
     } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Hmmm, something smells... No socks for you! :(");
@@ -75,14 +88,22 @@ app.get('/socks/:color', async (req, res) => {
 
 app.delete('/socks/:id', async (req, res) => {
     try {
-        const { id } = req.params;
-        console.log('Deleting sock with ID:', id);
-        res.status(200).send('Sock deleted successfully');
+        const {id} = req.params;
+        console.log(id);
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const result = await collection.deleteOne({ _id: new ObjectId(id) });
+        console.log(`${result.deletedCount} document(s) deleted`);
+        client.close();
+        res.json(result);
     } catch (err) {
         console.error('Error:', err);
         res.status(500).send('Hmm, something doesn\'t smell right... Error deleting sock');
     }
 });
+
+
 
 app.put('/user/:id', async (req, res) => {
     try {
